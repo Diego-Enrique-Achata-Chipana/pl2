3b7546435bcd90a92fad6d8b2c88f05ef168bc6b
Diego-Enrique-Achata-Chipana <diegoachata@upeu.edu.pe>
2020-08-20 21:39:25 -0500

    Actualizando Grado Controller

--- app/Http/Controllers/GradoController.php
+++ app/Http/Controllers/GradoController.php
@@ -12,7 +12,7 @@ class GradoController extends Controller
     public function index()
     {
         $grados = Grado::all();
-        return view('grado.index', compact($grados));
+        return view('grado.index', ['grados' => $grados]);
     }
 
     public function create(Request $request)
